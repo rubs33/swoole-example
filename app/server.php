b376f8336e2b52e36470c2afdae7ecbe41a19957
Rubens Takiguti Ribeiro <rubens.ribeiro@printi.com.br>
2020-07-31 10:41:50 -0300

    Initial commit

---
+++ app/server.php
@@ -0,0 +1,15 @@
+<?php
+$http = new \Swoole\HTTP\Server('0.0.0.0', getenv('APP_PORT') ?? 3000);
+
+$http->on('start', static function ($server) {
+    fwrite(STDOUT, "Swoole http server is started\n");
+});
+
+$http->on('request', function ($request, $response) {
+    $response->header('Content-Type', 'text/plain');
+    $response->end("Hello World 2\n");
+
+    fwrite(STDOUT, "REQUEST!\n");
+});
+
+$http->start();
